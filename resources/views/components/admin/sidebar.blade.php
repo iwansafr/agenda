eb408377cc3cea84a975e221872039eeb48fe7ae
esoftgreat <iwansafr@gmail.com>
2023-10-29 02:35:20 +0700

    Add bottom navigation

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -9,18 +9,18 @@
             </li>
             <li class="nav-item">
                 <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
-                    <i class="bi bi-menu-button-wide"></i><span>Product</span><i
+                    <i class="bi bi-menu-button-wide"></i><span>Agenda</span><i
                         class="bi bi-chevron-down ms-auto"></i>
                 </a>
                 <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
                     <li>
-                        <a href="{{ url('admin/product/add') }}">
-                            <i class="bi bi-circle"></i><span>Add Product</span>
+                        <a href="{{ url('/agenda/add') }}">
+                            <i class="bi bi-circle"></i><span>Add Agenda</span>
                         </a>
                     </li>
                     <li>
-                        <a href="{{ url('admin/product/list') }}">
-                            <i class="bi bi-circle"></i><span>Product List</span>
+                        <a href="{{ url('/agenda/list') }}">
+                            <i class="bi bi-circle"></i><span>Agenda List</span>
                         </a>
                     </li>
                 </ul>
@@ -29,7 +29,7 @@
             <li class="nav-heading">User</li>
 
             <li class="nav-item">
-                <a class="nav-link @if(url()->current() != url('admin/profile')) collapsed @endif" href="{{ url('admin/profile') }}">
+                <a class="nav-link @if(url()->current() != url('/profile')) collapsed @endif" href="{{ url('/profile') }}">
                     <i class="bi bi-person"></i>
                     <span>Profile</span>
                 </a>
