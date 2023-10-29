1ee3d01eaca06ab5908afe4ed381708bb1df0c6b
esoftgreat <iwansafr@gmail.com>
2023-10-30 06:07:00 +0700

    test

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'user_id',
+        'role_id',
         'name',
         'email',
         'password',
